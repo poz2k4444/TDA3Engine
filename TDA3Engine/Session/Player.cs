95ae2fcaa786e1818ca1a3ab98cf1715057f9e09
poz2k4444 <poz2k4444@gmail.com>
2013-10-07 18:09:26 -0500

    primer commit
    va

---
+++ TDA3Engine/Session/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDA3Engine
+{
+    public class Player
+    {
+        public uint Money
+        {
+            get;
+            set;
+        }
+
+        public List<Tower> PlacedTowers
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ TDA3Game/TDA3Game/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDA3Game
+{
+    public static class Settings
+    {
+        public static float MusicVolume;
+        public static float SoundVolume;
+    }
+}
